bf1bb59e0e430b08ad2beca2f9edacab07558a4b
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-03 22:49:34 +0200

    Adds a message handler and a message processing service

---
+++ src/main/java/com/example/dispatch/service/DispatchService.java
@@ -0,0 +1,10 @@
+package com.example.dispatch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DispatchService {
+    public void process(String payload) {
+
+    }
+}
---
+++ src/test/java/com/example/dispatch/handler/OrderCreatedHandlerTest.java
@@ -0,0 +1,25 @@
+package com.example.dispatch.handler;
+
+import com.example.dispatch.service.DispatchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class OrderCreatedHandlerTest {
+    private OrderCreatedHandler handler;
+    private DispatchService dispatchServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
+    }
+
+    @Test
+    void listen() {
+        handler.listen("payload");
+        verify(dispatchServiceMock, times(1)).process("payload");
+    }
+}

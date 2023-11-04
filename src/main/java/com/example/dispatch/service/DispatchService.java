3b8fcf9d9f90c84ca12e1089b4f8e69f35ee4652
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-04 12:40:36 +0200

    Refactors for readability

--- src/main/java/com/example/dispatch/service/DispatchService.java
+++ src/main/java/com/example/dispatch/service/DispatchService.java
@@ -6,15 +6,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+
 @Service
 @RequiredArgsConstructor
 public class DispatchService {
     public static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private final KafkaTemplate<String, Object> kafkaTemplate;
     public void process(OrderCreated orderCreated) throws Exception {
-        OrderDispatched orderDispatched = OrderDispatched.builder()
+        OrderDispatched orderDispatched = getOrderDispatchedEvent(orderCreated);
+        sendOrderDispatchedEvent(orderDispatched);
+    }
+
+    private static OrderDispatched getOrderDispatchedEvent(OrderCreated orderCreated) {
+        return OrderDispatched.builder()
                 .orderId(orderCreated.getOrderId())
                 .build();
+    }
+
+    private void sendOrderDispatchedEvent(OrderDispatched orderDispatched) throws InterruptedException, ExecutionException {
         kafkaTemplate.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
     }
 }

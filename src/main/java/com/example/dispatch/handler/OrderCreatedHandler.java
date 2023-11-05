57343be0a0330b5ae2b098ff08ee6f68970bd411
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-05 16:08:13 +0200

    Adds the dispatch completed event

--- src/main/java/com/example/dispatch/handler/OrderCreatedHandler.java
+++ src/main/java/com/example/dispatch/handler/OrderCreatedHandler.java
@@ -1,9 +1,11 @@
 package com.example.dispatch.handler;
 
+import com.example.message.DispatchCompleted;
 import com.example.message.OrderCreated;
 import com.example.dispatch.service.DispatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -13,15 +15,15 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@KafkaListener(
+        id = "orderConsumerClient",
+        topics = "order.created",
+        groupId = "dispatch.order.created.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class OrderCreatedHandler {
 
     private final DispatchService dispatchService;
-    @KafkaListener(
-            id = "orderConsumerClient",
-            topics = "order.created",
-            groupId = "dispatch.order.created.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
     public void listen(
             @Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition,
             @Header(KafkaHeaders.RECEIVED_KEY) String key,
@@ -33,6 +35,5 @@ public class OrderCreatedHandler {
         } catch (Exception e) {
             log.error("Processing failure", e);
         }
-
     }
 }
--- src/main/java/com/example/dispatch/service/DispatchService.java
+++ src/main/java/com/example/dispatch/service/DispatchService.java
@@ -1,5 +1,6 @@
 package com.example.dispatch.service;
 
+import com.example.message.DispatchCompleted;
 import com.example.message.DispatchPreparing;
 import com.example.message.OrderCreated;
 import com.example.message.OrderDispatched;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -25,6 +27,8 @@ public class DispatchService {
         sendOrderDispatchedEvent(key, orderDispatched);
         DispatchPreparing dispatchPreparingEvent = getDispatchPreparingEvent(orderCreated);
         sendDispatchPreparingEvent(key, dispatchPreparingEvent);
+        DispatchCompleted dispatchCompletedEvent = getDispatchCompletedEvent(orderCreated);
+        sendDispatchCompleted(key, dispatchCompletedEvent);
         log.info("Sent messages: key: " + key + " orderId: " + orderCreated.getOrderId() + " - processedById: " + APPLICATION_ID);
     }
 
@@ -48,5 +52,17 @@ public class DispatchService {
 
     private void sendDispatchPreparingEvent(String key, DispatchPreparing dispatchPreparingEvent) throws InterruptedException, ExecutionException {
         kafkaTemplate.send(DISPATCH_TRACKING_TOPIC, key, dispatchPreparingEvent).get();
+    }
+
+    private DispatchCompleted getDispatchCompletedEvent(OrderCreated orderCreated) {
+        return DispatchCompleted
+                .builder()
+                .orderId(orderCreated.getOrderId())
+                .date(new Date().toString())
+                .build();
+    }
+
+    private void sendDispatchCompleted(String key, DispatchCompleted dispatchCompletedEvent) throws ExecutionException, InterruptedException {
+        kafkaTemplate.send(DISPATCH_TRACKING_TOPIC, key, dispatchCompletedEvent).get();
     }
 }
